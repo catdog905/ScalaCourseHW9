6374ffd9c4629ac47aac594a57d992cc725556b8
Yaroslav <y.kivaev@gmail.com>
2023-11-16 22:30:59 +0800

    Stack-unsafe IO

--- src/main/scala/io/IO.scala
+++ src/main/scala/io/IO.scala
@@ -1,34 +1,60 @@
 package io
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
-final class IO[A] {
-  def map[B](f: A => B): IO[B] = ???
-  def flatMap[B](f: A => IO[B]): IO[B] = ???
-  def *>[B](another: IO[B]): IO[B] = ???
-  def as[B](newValue: => B): IO[B] = ???
-  def void: IO[Unit] = ???
-  def attempt: IO[Either[Throwable, A]] = ???
-  def option: IO[Option[A]] = ???
-  def handleErrorWith[AA >: A](f: Throwable => IO[AA]): IO[AA] = ???
-  def redeem[B](recover: Throwable => B, map: A => B): IO[B] = ???
-  def redeemWith[B](recover: Throwable => IO[B], bind: A => IO[B]): IO[B] = ???
-  def unsafeRunSync(): A = ???
+final class IO[A](val impureCompute: () => A) {
+  def map[B](f: A => B): IO[B] = new IO(() => f(unsafeRunSync()))
+  def flatMap[B](f: A => IO[B]): IO[B] = new IO(() => {
+    val intermediate: A = unsafeRunSync()
+    f(intermediate).unsafeRunSync()
+  })
+  def *>[B](another: IO[B]): IO[B] = flatMap(_ => another)
+  def as[B](newValue: => B): IO[B] = map(_ => newValue)
+  def void: IO[Unit] = map(_ => ())
+  def attempt: IO[Either[Throwable, A]] = new IO(() => Try(unsafeRunSync()).toEither)
+  def option: IO[Option[A]] = attempt.map(_.toOption)
+  def handleErrorWith[AA >: A](f: Throwable => IO[AA]): IO[AA] =
+    redeemWith(f, IO.pure)
+  def redeem[B](recover: Throwable => B, map: A => B): IO[B] =
+    attempt.map({
+      case Left(throwable) => recover(throwable)
+      case Right(value)    => map(value)
+    })
+
+  def redeemWith[B](recover: Throwable => IO[B], bind: A => IO[B]): IO[B] =
+    attempt.flatMap({
+      case Left(throwable) => recover(throwable)
+      case Right(value)    => bind(value)
+    })
+  def unsafeRunSync(): A = impureCompute()
 }
 
 object IO {
-  def apply[A](body: => A): IO[A] = ???
-  def suspend[A](thunk: => IO[A]): IO[A] = ???
-  def delay[A](body: => A): IO[A] = ???
-  def pure[A](a: A): IO[A] = ???
-  def fromEither[A](e: Either[Throwable, A]): IO[A] = ???
-  def fromOption[A](option: Option[A])(orElse: => Throwable): IO[A] = ???
-  def fromTry[A](t: Try[A]): IO[A] = ???
-  def none[A]: IO[Option[A]] = ???
-  def raiseError[A](e: Throwable): IO[A] = ???
-  def raiseUnless(cond: Boolean)(e: => Throwable): IO[Unit] = ???
-  def raiseWhen(cond: Boolean)(e: => Throwable): IO[Unit] = ???
-  def unlessA(cond: Boolean)(action: => IO[Unit]): IO[Unit] = ???
-  def whenA(cond: Boolean)(action: => IO[Unit]): IO[Unit] = ???
-  val unit: IO[Unit] = ???
+  def apply[A](body: => A): IO[A] = new IO(() => body)
+  def suspend[A](thunk: => IO[A]): IO[A] = thunk
+  def delay[A](body: => A): IO[A] = IO(body)
+  def pure[A](a: A): IO[A] = IO(a)
+  def fromEither[A](e: Either[Throwable, A]): IO[A] = e match {
+    case Left(exception) => IO.raiseError(exception)
+    case Right(value)    => IO.pure(value)
+  }
+  def fromOption[A](option: Option[A])(orElse: => Throwable): IO[A] = option match {
+    case Some(value) => IO.pure(value)
+    case None        => raiseError(orElse)
+  }
+  def fromTry[A](t: Try[A]): IO[A] = t match {
+    case Success(value)     => IO.pure(value)
+    case Failure(exception) => IO.raiseError(exception)
+  }
+  def none[A]: IO[Option[A]] = pure(None)
+  def raiseError[A](e: Throwable): IO[A] = IO.delay(throw e)
+  def raiseUnless(cond: Boolean)(e: => Throwable): IO[Unit] = whenA(!cond)(raiseError(e))
+  def raiseWhen(cond: Boolean)(e: => Throwable): IO[Unit] = whenA(cond)(raiseError(e))
+  def unlessA(cond: Boolean)(action: => IO[Unit]): IO[Unit] = whenA(!cond)(action)
+  def whenA(cond: Boolean)(action: => IO[Unit]): IO[Unit] = if (cond) {
+    action
+  } else {
+    IO.unit
+  }
+  val unit: IO[Unit] = pure(())
 }
